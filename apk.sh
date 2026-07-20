9c1c4601de082b4dc12743a531521b77960ad659
tony <tony@gmail.com>
2026-07-20 18:16:10 +0700

    更新版本1.0.3

--- apk.sh
+++ apk.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$0")"
 
 echo ">>> 正在打包 Android APK（arm64，混淆+剥离符号，仅新系统 minSdk 30）..."
 # ✅ 关键：添加 --release
+flutter clean
+flutter pub get
 flutter build apk \
   --release \
   --target-platform=android-arm64 \
